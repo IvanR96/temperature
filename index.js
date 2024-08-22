85101665f4d29bf6660f3552136f8395fa4911bb
IvanR96 <irios96@hotmail.com>
2024-08-21 23:04:05 -0700

    ejs server started

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://api.weather.gov";
+
+app.use(express.static('public'));
+
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs", { content: "Welcome to EJS"});
+});
+
+
+app.listen(port, () =>{
+    console.log(`Server running on port ${port}...`);
+});
